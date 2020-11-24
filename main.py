15528f411e59903f5eccce757fa07659bcb9dffe
danikfml <danikfml@yandex.ru>
2020-11-24 15:41:51 +0400

    Файл main

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-import sys, random
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QMainWindow
+from PyQt5.QtGui import *
+from UI import Ui_MainWindow
+import sys
+import random
 
-from PyQt5 import uic
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
-
-class Example(QWidget):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
-        uic.loadUi('UI.ui', self)
-        self.btn.clicked.connect(self.paint)
+        self.setupUi(self)
+        self.setGeometry(500, 500, 800, 600)
+        self.setWindowTitle('Git и случайные окружности')
+        self.place = 0, 0, 0, 0
+        self.pushButton.clicked.connect(self.draw)
 
-    def initUI(self):
-        self.setWindowTitle('Рисование')
-        self.do_paint = False
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QColor(random.randint(1, 255), random.randint(1, 255),
+                           random.randint(1, 255)))
+        qp.drawEllipse(*self.place)
+        qp.end()
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def draw_flag(self, qp):
-        x, y = random.randint(0, 200), random.randint(0, 200)
-        x1, y1 = x, y
-        qp.setBrush(QColor(250, 250, 0))
-        qp.drawEllipse(x, y, x1, y1)
+    def draw(self):
+        rad = random.randint(50, 500)
+        self.place = random.randint(50, 500), random.randint(50, 500), rad, rad
+        self.update()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Window()
     ex.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
